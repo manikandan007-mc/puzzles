2a313b3f47f2a2f5464b22d12f24f46f99d16a0d
manikandan007-mc <manickfi188@gmail.com>
2026-06-02 16:39:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     levels: [
-        { id: 1, image: 'image1.png' },
-        { id: 2, image: 'image2.png' },
-        { id: 3, image: 'image3.png' }
+        { id: 1, image:'image1.png' },
+        { id: 2, image:'image2.png' },
+        { id: 3, image:'image3.png' }
     ],
     gridSize: 3
 };
